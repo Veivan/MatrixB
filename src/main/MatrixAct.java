89a15a27862590ff23ced0e7927961539a3e682a
veivan <veivan@yandex.ru>
2016-11-20 07:53:03 +0300

    Remove redundant properties

--- src/main/MatrixAct.java
+++ src/main/MatrixAct.java
@@ -10,17 +10,11 @@ public class MatrixAct {
 	private JobAtom job;
 	private IAccount acc;
 
-	private long AccID;
-	private String ActionTXT;
-
 	private long ID;
 
 	public MatrixAct(JobAtom ajob, IAccount acc) {
 		this.job = new JobAtom(ajob);
 		this.acc = acc;
-
-		this.AccID = acc.getAccID();
-		this.setActionTXT(ajob.Type.name());
 
 		Random random = new Random();
 		this.ID = random.nextInt();
@@ -34,20 +28,8 @@ public class MatrixAct {
 		return acc;
 	}
 
-	public long getAccID() {
-		return AccID;
-	}
-
 	public long getSelfID() {
 		return this.ID;
-	}
-
-	public String getActionTXT() {
-		return ActionTXT;
-	}
-
-	private void setActionTXT(String actionTXT) {
-		ActionTXT = actionTXT;
 	}
 
 }
