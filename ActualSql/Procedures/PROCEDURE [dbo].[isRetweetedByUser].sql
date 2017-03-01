1e0a1d0cb5dfaf63609ad0d655aa074eab3c178a
veivan <veivan@yandex.ru>
2017-03-01 12:48:34 +0300

    isRetweetedByUser fixed

--- ActualSql/Procedures/PROCEDURE [dbo].[isRetweetedByUser].sql
+++ ActualSql/Procedures/PROCEDURE [dbo].[isRetweetedByUser].sql
@@ -2,10 +2,6 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
--- ================================================
--- Author:	Vetrov
--- Description:	Check if Status retweeted by user
--- ================================================
 ALTER PROCEDURE [dbo].[isRetweetedByUser]
 	@tw_id BIGINT
 	,@user_id BIGINT
@@ -14,9 +10,10 @@ AS BEGIN
 	DECLARE @result BIT
 	SET @result = 0
 	SELECT * 
-	FROM [dbo].[twTwits] 
-	WHERE [retweeted_id] = @tw_id
-		AND [creator_id] = @user_id
+	FROM [dbo].[twTwits] T
+		INNER JOIN [dbo].[mAccounts] A ON A.[twitter_id] = T.[creator_id]
+	WHERE T.[retweeted_id] = @tw_id
+		AND A.[user_id] = @user_id
 	IF @@ROWCOUNT > 0
 		SET @result = 1
 	SELECT @result as result
